41946c86a433ed2df1d6985247a7e7106d94bc23
Mathis Degryck <madegryc@3F6.42angouleme.fr>
2024-10-13 18:44:04 +0200

    Add sig serv

--- main.cpp
+++ main.cpp
@@ -6,11 +6,30 @@
 /*   By: madegryc <madegryc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:55:46 by madegryc          #+#    #+#             */
-/*   Updated: 2024/10/13 18:17:46 by madegryc         ###   ########.fr       */
+/*   Updated: 2024/10/13 18:42:35 by madegryc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server/server.hpp"
+
+void closeServer(Server *server)
+{
+    static Server *serv = NULL;
+
+    if (serv == NULL)
+    {
+        serv = server;
+        return ;
+    }
+    exit(0);
+}
+
+void sigintHandler(int sig)
+{
+    (void)sig;
+    std::cout << "Caught signal " << std::endl;
+    closeServer(NULL);
+}
 
 int main(int ac, char **av)
 {
@@ -31,15 +50,19 @@ int main(int ac, char **av)
         std::cerr << e.what() << std::endl;
         return 1;
     }
-    // if (signal(SIGINT, sigintHandler) == SIG_ERR)
-	// {
-	// 	std::cerr << "Error: can't catch SIGINT" << std::endl;
-	// 	delete (server);
-	// 	return (1);
-	// }
+    
+    closeServer(&server);
+    
+    if (signal(SIGINT, sigintHandler) == SIG_ERR)
+	{
+		std::cerr << "Error: can't catch SIGINT" << std::endl;
+		return (1);
+	}
+    
     while (true)
     {
         server.acceptClient();
     }
+    
     return 0;
 }
